ad42ae766d96f359242df16865e28ddf7bf446b8
Gilles <chekunive@gmail.com>
2022-07-08 23:47:55 +0200

    initial

--- src/main/java/nombres/FonctionToTextFile.java
+++ src/main/java/nombres/FonctionToTextFile.java
@@ -10,7 +10,7 @@ public class FonctionToTextFile {
     public static void fonctionToTextFile(Map<Integer, Integer> F, String fileaddr, String filename, int n) throws IOException {
         StringBuilder sb = new StringBuilder();
         F.forEach((key, value) -> sb.append(key).append(",").append(value).append("\n"));
-System.out.println(sb);
+        System.out.println(sb);
         FileWriter fw = new FileWriter(fileaddr + filename + n + ".txt", false);
         BufferedWriter output = new BufferedWriter(fw);
         output.write(sb.toString());
---
+++ src/main/java/nombres/base/simple/MatricesSimple3.java
@@ -0,0 +1,69 @@
+package nombres.base.simple;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.pow;
+import static java.lang.System.arraycopy;
+import static java.util.stream.IntStream.range;
+import static nombres.FonctionToTextFile.fonctionToTextFile;
+import static nombres.MatriceToTextFile.matriceToTextFile;
+
+public class MatricesSimple3 extends ArrayList<MatricesSimple3.Paire> {
+
+    private final int N = 256;
+    private final int[][] tab = new int[N + 1][N + 1];
+
+    //  String chemin = "/home/tdc/IdeaProjects/LesNombres2/src/main/java/nombres/base/simple/";
+    String chemin = "C:\\Users\\gille\\IdeaProjects\\LesNombres2" +
+            "\\src\\main\\java\\nombres\\base\\simple\\";
+
+    public MatricesSimple3() throws IOException {
+
+        int[][] f = f();
+        int[] valeurs = IntStream.of(to1Dtab(f)).distinct().sorted().toArray();
+        System.out.println(Arrays.toString(valeurs));
+        Map<Integer, Integer> f1 = new HashMap<>();
+        //int val=2;
+        for (int val : valeurs) {
+            int count = 0;
+            for (int i = 1; i < N; i++) {
+                for (int j = 1; j < N; j++) {
+                    //System.out.print(f[i][j]+",");
+                    if (f[i][j] == val) count++;
+                }
+                //System.out.println();
+            }
+            f1.put(val, count);
+        }
+        //System.out.println(Arrays.toString(f));
+        //System.out.println(count);
+        matriceToTextFile(f, chemin, "f_", N);
+        fonctionToTextFile(f1, chemin, "F1D_", N);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new MatricesSimple3();
+    }
+
+    private int[][] f() {
+        for (int i = 1; i <= N; i++) for (int j = 1; j <= N; j++) add(new Paire(i, j));
+        for (Paire p : this) for (Paire q : this) if (p.i * p.j == q.i * q.j) tab[p.i][p.j]++;
+        return tab;
+    }
+
+    int[] to1Dtab(int[][] tab2D) {
+        int[] tab1D = new int[(int) pow(tab2D.length, 2)];
+        range(1, tab2D.length).forEach(i -> arraycopy(tab2D[i],
+                0, tab1D, i * tab2D.length, tab2D.length));
+        return tab1D;
+    }
+
+    public record Paire(Integer i, Integer j) {
+    }
+}
+
